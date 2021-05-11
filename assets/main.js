b7a21052ab1c51be1eaba884d8977289ec3ecdd9
alexellevu <newnapster83@gmail.com>
2021-05-11 16:41:16 +0200

    creazione array bici

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+//Creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+//nome e
+//peso.
+//Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+var bici = [
+    bici1 = {nome: 'santa cruz', peso:10},
+    bici2 = {nome: 'trek', peso:11},
+    bici3 = {nome:'merida', peso:9},
+    bici4 = {nome:'kona', peso:12},
+    bici5 = {nome:'giant', peso: 8}
+];
+console.log(bici1.peso);
+
+/* var pesi =[];
+for(var i = 0; i <bici.length;i++){
+    console.log(bici.peso[i]);
+   pesi.push(bici.peso[i]);
+}
+console.log(pesi);
+
+/* var min = Math.min(bici.peso);
+console.log(min); */
