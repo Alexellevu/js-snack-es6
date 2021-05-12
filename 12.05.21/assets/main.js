2768f006094c84c3240c73d4dde2ea0844731556
alexellevu <newnapster83@gmail.com>
2021-05-12 17:11:51 +0200

    correzione for heach e filter

--- 12.05.21/assets/main.js
+++ 12.05.21/assets/main.js
@@ -4,25 +4,30 @@
 //:collisione: Usiamo i nuovi metodi degli array foreach o filter.
 
 const array = [1,2,35,41,18,10,30,40];
-let a = 1;
-const b = 6 ;
+const a = 5;
+const b = 6;
 
 const arrayFinale =[];
-/* array.forEach((element,index) => {
-    while(a < b){
-    arrayFinale.push(array[a]);
-    a++;
+array.forEach((element,index) => {
+    if(index >= a && index <= b){
+    arrayFinale.push(element);
+    
     }
-
 }
 ); 
-console.log(arrayFinale); */
+console.log(arrayFinale);
+for(var i=0; i<arrayFinale.length; i++){
+    document.getElementById('stampa').innerHTML += arrayFinale[i] + '<br>';
+} 
 
-const arrayfilter = array.filter((a) => {
-    while(a < b){
-        arrayFinale.push(array[a]);
-        a++;
+const arrayfilter = array.filter((element, index) => {
+     if(index >= a && index <= b){
+    return true;
     }
 });
 
-console.log(arrayFinale);
+console.log(arrayfilter);
+
+for(var i=0; i<arrayfilter.length; i++){
+    document.getElementById('stampa2').innerHTML += arrayfilter[i] + '<br>';
+} 
