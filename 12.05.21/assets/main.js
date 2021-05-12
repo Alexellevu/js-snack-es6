98ab931386806aeabbbca7427fc39f533096948e
alexellevu <newnapster83@gmail.com>
2021-05-12 16:06:11 +0200

    soluzione con for heach

--- 12.05.21/assets/main.js
+++ 12.05.21/assets/main.js
@@ -3,3 +3,19 @@
 //La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 //:collisione: Usiamo i nuovi metodi degli array foreach o filter.
 
+const array = [1,2,35,41,18,10,30,40];
+var a = 5;
+const b = 6 ;
+
+const arrayFinale =[];
+array.forEach((element,index) => {
+    while(a <= b){
+    arrayFinale.push(array[a]);
+    a++
+    }
+
+}
+
+); 
+
+console.log(arrayFinale);
